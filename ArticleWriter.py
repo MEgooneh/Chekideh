fcfd3b14bae0de78bf30cbfd1409f8ac599b5671
MEgooneh <mohammad.erfan.gooneh@gmail.com>
2023-08-07 23:05:16 +0330

    Write initial codes

--- ArticleWriter.py
+++ ArticleWriter.py
@@ -1,1 +1,33 @@
-import json, api
+import json, api, requests
+
+def get_book_details(book_title) : 
+    url = f"https://www.googleapis.com/books/v1/volumes?q={book_title}&maxResults=1"
+    response = requests.get(url)
+    data = response.json()
+    if "items" not in data or len(data["items"]) == 0:
+        return 0
+    return data['items'][0]['volumeInfo']
+
+with open('articles_status.json' , 'r') as file :
+    articles = json.load(file)
+articles = [articles[0]]
+ind = 0
+for article in articles :
+    if article['written'] == 0 : 
+        try:
+            with open('writing-format-prompt.txt','r') as file :
+                res = api.send_message(eval(file.read()))  
+            data = get_book_details(article['name']) 
+            data['imageLinks']['thumbnail'] = 'https://www.drupal.org/files/issues/2019-07-21/missing.png' if ("imageLinks" not in data) or ("thumbnail" not in data["imageLinks"]) else data['imageLinks']['thumbnail']
+            article['written'] = 1
+            with open('articles_status.json','r') as file : 
+                articles[ind] = article
+                json.dump(articles , file)
+            with open('post-format.txt' , 'r') as file :
+                res = eval(file.read())
+            with open(f"docs/{article['category']}/{data['title']}.md" , 'w') as file : 
+                file.write(res)
+            print(f"Article {article['name']} Done!")
+        except Exception as e:
+            print(f"Error while writing article {article['name']}. : {e}")
+    ind += 1
--- api.py
+++ api.py
@@ -1,7 +1,6 @@
 # CONFIG YOUR API HERE : 
 RATE_LIMIT = 20 # sleeping time for openai per minute limitation
 TOKEN_LIMIT = 0 # token limit per message 0=inf
-TEMPERATURE = 1
 MODEL = 'gpt-3.5-turbo'
 
 import openai, os, time
@@ -27,7 +26,6 @@ def send_message(message , token_limit=TOKEN_LIMIT , time_limit_rate=RATE_LIMIT)
     response = openai.ChatCompletion.create(
         model=MODEL,
         messages=context,
-        temperature=TEMPERATURE
     )
     # just for debugging in terminal
     print(f"""
